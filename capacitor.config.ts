177d66b19571be8c73f2652090706732fb3075f4
arshad5517 <arshadansarima520@gmail.com>
2026-05-07 15:46:19 +0530

    Update Capacitor configuration for new app settings

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,6 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.government.schemes',
-  appName: 'Government Schemes India',
-  webDir: 'www'
-};
-
-export default config;
+{
+  "appId": "io.ionic.starter",
+  "appName": "Sarkari Yojana",
+  "webDir": "www",
+  "bundledWebRuntime": false
+}
