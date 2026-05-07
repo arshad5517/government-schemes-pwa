cd8490cbcd23a2c3d52d9c71e96a0635078f4f00
arshad5517 <arshadansarima520@gmail.com>
2026-05-07 22:39:25 +0530

    fixed github pages

---
+++ android/app/src/main/java/com/sarkariyojana/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.sarkariyojana.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sarkariyojana.app',
+  appName: 'government-schemes-pwa',
+  webDir: 'www',
+};
+
+export default config;
